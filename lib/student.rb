236ab57cdea01b3397416d0634586e58611688e7
clean-command-1851 <ed7901979t@gmail.com>
2019-09-11 18:11:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,27 @@
-class Student 
-
+class Student < User
+  attr_accessor :student
+  
+   KNOWLEDGE = []
+  
+  def initialize
+    @student=student
+     KNOWLEDGE << self
+  end
+  
+  def self.KNOWLEDGE
+    KNOWLEDGE
+    
+  end
+  
+  def learn(new_item)
+     
+    KNOWLEDGE.push(new_item)
+    
+  
+  end
+  
+  def knowledge   
+  return KNOWLEDGE
+  end
+  
 end
